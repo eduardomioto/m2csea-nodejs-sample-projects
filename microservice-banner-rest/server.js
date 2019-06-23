550ab71fd23c7ab20e743fcda2e7840b30df90da
Eduardo Mioto <eduardo.mioto@gmail.com>
2019-06-23 11:06:04 -0300

    Add files via upload

---
+++ microservice-banner-rest/server.js
@@ -0,0 +1,81 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var Consul = require('consul');
+
+var consulUrl = "127.0.0.1";
+console.log('consulUrl: '+ consulUrl);
+
+app.get('/', function(req, res){
+
+    var objToJson = {
+        purpose: "Banner"
+    };
+    res.setHeader('content-type', 'text/javascript');
+    res.send(objToJson);
+});
+
+app.get('/banner', function(req, res){
+
+    var objToJson = {
+        purpose: "Banner Endpoint"
+    };
+    res.setHeader('content-type', 'text/javascript');
+    res.send(objToJson);
+});
+
+http.listen(10002, function(){
+    console.log('Listening on *:10002');
+    console.log('http://localhost:10002/');
+    console.log('http://localhost:10002/banner');
+});
+
+var consul = new Consul({
+    host: consulUrl,
+    port: 8500
+});
+
+var service = "microservice-banner-rest";
+
+var check = {
+    name: service,
+    ttl: '15s',
+    notes: 'This is an example check.'
+};
+
+consul.agent.service.register(service, function(err) {
+    if (err) throw err;
+});
+
+consul.health.service(service, function(err, result) {
+    if (err) throw err;
+});
+
+consul.agent.check.register(check, function(err) {
+    if (err) throw err;
+});
+
+consul.agent.check.deregister(service, function(err) {
+    if (err) throw err;
+});
+
+consul.agent.check.pass(service, function(err) {
+    if (err) throw err;
+});
+
+var kvValue = {
+    "version":"1.0",
+    "dependencies":[
+        {
+            "name":"microservice-calendar-rest",
+            "version":"1.0"
+        },
+        {
+            "name":"microservice-access-manager-rest",
+            "version":"1.0"
+        }
+    ]
+}
+
+consul.kv.set(service, JSON.stringify(kvValue), function(err, result) {
+    if (err) throw err;
+});

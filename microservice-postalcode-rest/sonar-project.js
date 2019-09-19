dbf258814b17a7f2a498c56538d2ec3a56b31c55
Eduardo Mioto <eduardo.mioto@philips.com>
2019-09-19 14:23:49 -0300

    update sonar
    update sonar

--- microservice-postalcode-rest/sonar-project.js
+++ microservice-postalcode-rest/sonar-project.js
@@ -3,6 +3,6 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://localhost:9000',
        options : {
        'sonar.sources': '.',
-       'sonar.inclusions' : 'src/**' // Entry point of your code
+       'sonar.inclusions' : './**' // Entry point of your code
        }
      }, () => {});

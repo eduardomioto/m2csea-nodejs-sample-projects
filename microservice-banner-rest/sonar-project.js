7fca9e4f6cb788f04d44d6bbae85a5c8de6ddb45
Eduardo Mioto <eduardo.mioto@philips.com>
2019-09-19 08:37:03 -0300

    update projects
    update projects

---
+++ microservice-banner-rest/sonar-project.js
@@ -0,0 +1,8 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+     sonarqubeScanner({
+       serverUrl: 'http://localhost:9000',
+       options : {
+       'sonar.sources': '.',
+       'sonar.inclusions' : 'src/**' // Entry point of your code
+       }
+     }, () => {});

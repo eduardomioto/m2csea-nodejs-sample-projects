f2c77650ecb9640b57585e6f07edc287cb3fe7a7
Eduardo Mioto <eduardo.mioto@philips.com>
2019-10-19 13:56:31 -0300

    update

--- microservice-banner-rest/server.js
+++ microservice-banner-rest/server.js
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
     port: '3306',
     user: 'root',
     password: 'admin',
-    database: 'mc2sea-metrics   '
+    database: 'm2csea-metrics   '
 });
 
 function saveMetrics(microservice, responseTime) {
